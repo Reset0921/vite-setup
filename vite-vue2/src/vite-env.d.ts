26fd057fc84c4dc5dcd443a2bcaa23d0e982b644
王江 <1414798586@qq.com>
2022-03-28 10:04:11 +0800

    init vite-vue2

---
+++ vite-vue2/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: Object
+    export default component
+  }
+  
